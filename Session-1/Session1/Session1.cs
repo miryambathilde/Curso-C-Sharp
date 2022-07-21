320781ec86a5bdef0f634e07ebf5930d56bed1ab
MiryamBathilde <mbathilde@gmail.com>
2022-07-21 18:33:39 +0200

    exercise session 1 done

---
+++ Session-1/Session1/Session1.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Nombre?
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+
+
+// Edad?
+Console.WriteLine("Indica tu edad: ");
+string edad = Console.ReadLine();
+
+// Ciudad?
+Console.WriteLine("Introduce tu ciudad: ");
+string ciudad = Console.ReadLine();
+
+// Presentar mensaje "Hola, NOMBRE. Tienes AÑOS. Vives en CIUDAD
+Console.WriteLine("Hola, " + nombre + ". Tienes " + edad + " años. Vives en " + ciudad);
